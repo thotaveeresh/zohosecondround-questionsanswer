26c73fbc56883f1dde616ce1c9229a5aa853725e
thotaveeresh <59527917+thotaveeresh@users.noreply.github.com>
2021-01-11 09:28:54 +0530

    Create python1.py

---
+++ python1.py
@@ -0,0 +1,19 @@
+n=int(input("Enter pattern:"))
+for i in range(n):
+    if i==0:
+        print('5 5 5 5 5')
+    elif i==1:
+        print('5 4 4 4 4')
+    elif i==2:
+        print("5 4 3 3 3")
+    elif i==3:
+        print("5 4 3 2 2")
+    else:
+        print("5 4 3 2 1")
+
+
+
+
+
+
+
